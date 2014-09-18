a808e3fe658c7862f33717d8cdf32e7a97755fe3
John Haugeland <stonecypher@gmail.com>
2014-09-18 00:41:46 -0700

    draft

---
+++ node-validate-email.js
@@ -0,0 +1,8 @@
+
+// https://fightingforalostcause.net/content/misc/2006/compare-email-regex.php
+
+function is_email(addr) {
+    return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(addr);
+}
+
+module.exports = is_email;
